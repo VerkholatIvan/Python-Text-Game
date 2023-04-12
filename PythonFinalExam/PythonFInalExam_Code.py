d3acfdfacc539fc33fa0b133919d63a33d10199c
VerkholatIvan <123458146+VerkholatIvan@users.noreply.github.com>
2023-04-12 12:39:40 +0100

    Buy armour ability and adding to the inventory function with the inventory changes
    - "Add to the inventory function"  Adds different items based on their types, which are: "Weapons", "Keys" and "Armour" with their own indicators (e.g.  damage,  price, durability, ...).
    
    - The "inventory" variable was changed from usual dictionary to dictionary with sections.
    
    - Commentaries was added for better navigation.

--- PythonFinalExam/PythonFInalExam_Code.py
+++ PythonFinalExam/PythonFInalExam_Code.py
@@ -7,10 +7,12 @@ main_window.title("Text based adventure game")
 main_window.geometry("400x400")
 # name = input("Enter your name >> ")
 name = "Ivan"
-money = random.randint(50, 310)
+# money = random.randint(50, 310)
+money = 500
 point = 0
 health = 100
-inventory = {}
+
+inventory = {"Weapons":[], "Keys":[], "Armours":[]}
 
 top_frame = tkinter.Frame(main_window)
 top_frame.pack(anchor = "n")
@@ -24,12 +26,13 @@ money_label.grid(row=0, column=1)
 point_label = tkinter.Label(top_frame, text = "Points: "+str(point), font= ("Calibri, 16"))
 point_label.grid(row=1, column=1)
 
+bottom_frame = tkinter.Frame(main_window)
+bottom_frame.pack(anchor= "s")
 
-def change_labels(health, money, point):
-    health_label.configure(text= "Health: "+str(health))
-    money_label.configure(text= "Money: "+str(money))
-    point_label.configure(text= "Points: "+str(point))
 
+# ---------------------------------------------------------------
+# SHOP LISTS FOR REMOVAL    (´。＿。｀)
+# ---------------------------------------------------------------
 
 
 shop_weapon = []
@@ -41,6 +44,8 @@ with open("Shop.txt", "r") as dev_shop_update_weapon:
             shop_weapon.append(line.strip())
         i += 1
 
+#----------------------------
+
 shop_keys = []
 with open("Shop.txt", "r") as dev_shop_update_key:
     lines = np.arange(9, 11, 1)
@@ -50,26 +55,38 @@ with open("Shop.txt", "r") as dev_shop_update_key:
             shop_keys.append(line.strip())
         i += 1
 
+#----------------------------
 
+shop_armour = []
+with open("Shop.txt", "r") as dev_shop_update_armour:
+    lines = np.arange(15, 18, 1)
+    i = 0
+    for line in dev_shop_update_armour:
+        if i in lines:
+            shop_armour.append(line.strip())
+        i += 1
 
-bottom_frame = tkinter.Frame(main_window)
-bottom_frame.pack(anchor= "s")
+# ---------------------------------------------------------------
+# DEF MAIN FUNCTIONS    \(￣︶￣*\))
+# ---------------------------------------------------------------
 
 
 def Room1():
     # my_file = open("Room1.txt", "r")
     pass
+#----------------------------
 def Room2():
     pass
+#----------------------------
 def Room3():
     pass
+#----------------------------
 def Room4():
     pass
+#----------------------------
 def Shop():
-    global money
-    global health
-    global shop_weapon
-    global shop_keys
+    global money, health
+    global shop_weapon, shop_keys, shop_armour
 
     dev_shop = open("Shop.txt", "r")
     print("\n", dev_shop.readline())
@@ -88,13 +105,12 @@ def Shop():
 
         if user_buy == 1: # buy weapons
             print(*shop_weapon, sep="\n")
-            print("6) back")
             user_weapon_choose = input("Which of them you liked the most?\n>> ")
             if user_weapon_choose == "1" or user_weapon_choose == "weapon1":
                 if money >= 50:
                         money -= 50
                         change_labels(health, money, point)
-                        inventory["knife"] = 10
+                        add_item("knife,10,50", "w")
                         shop_weapon.remove("weapon1:knife,10,50")
 
                         print("Thanks for purchase")
@@ -105,8 +121,8 @@ def Shop():
                 if money >= 150:
                     money -= 150
                     change_labels(health, money, point)
-                    inventory["rifle"] = 30
-                    shop_weapon.remove("weapon2:rifle,30,150 ")
+                    add_item("rifle,30,150", "w")
+                    shop_weapon.remove("weapon2:rifle,30,150")
 
                     print("Thanks for purchase")
                 else:
@@ -116,7 +132,7 @@ def Shop():
                 if money >= 100:    
                     money -= 100
                     change_labels(health, money, point)
-                    inventory["pistol"] = 20
+                    add_item("pistol,20,100", "w")
                     shop_weapon.remove("weapon3:pistol,20,100")
 
                     print("Thanks for purchase")
@@ -127,7 +143,7 @@ def Shop():
                 if money >= 300:
                     money -= 300
                     change_labels(health, money, point)
-                    inventory["bomb"] = 50
+                    add_item("bomb,50,300", "w")
                     shop_weapon.remove("weapon4:bomb,50,300")
 
                     print("Thanks for purchase")
@@ -138,14 +154,12 @@ def Shop():
                 if money >= 250:    
                     money -= 250
                     change_labels(health, money, point)
-                    inventory["sniper"] = 40
+                    add_item("sniper,40,250", "w")
                     shop_weapon.remove("weapon5:sniper,40,250")
 
                     print("Thanks for purchase")
                 else:
                     print("Not enough cash")
-
-            # elif user_weapon_choose == "6" or user_weapon_choose == "back":
 
 
             else: print("\nThere is no such an item\nLook wisely")
@@ -158,38 +172,98 @@ def Shop():
                 if money >= 300:
                     money -= 300
                     change_labels(health, money, point)
-                    inventory["key"] = 0
+                    add_item("0,300", "k")
                     shop_keys.remove("key:0,300")
                     
                     print("Thanks for purchase")
                 else:
                     print("Not enough cash")
+
+            elif user_key_choose == "no" or user_key_choose == "0":
+                print("As you wish!\nBut still take a look on the other items")
+            
+            else:
+                print("Did not understand you")
                     
 
-        # elif user_buy == 3:
+        # elif user_buy == 3: # HealingPad
+            
 
-        # elif user_buy == 4:
+        elif user_buy == 4: # Armour
+            print(*shop_armour, sep="\n")
+            user_armour_choose = input("Which armour do you prefer?\n>> ")
 
-        elif user_buy == 5:
+            if user_armour_choose == "1": # choose armour 1
+                if money >= 200:
+                    money -= 200
+                    change_labels(health, money, point)
+                    add_item("2,200", "a")
+                    shop_armour.remove("armour1:2,200")
+
+                    print("Thanks for purchase")
+                else:
+                    print("Not enough cash")
+
+            elif user_armour_choose == "2": # choose armour 2
+                if money >= 400:
+                    money -= 400
+                    change_labels(health, money, point)
+                    add_item("3,400", "a")
+                    shop_armour.remove("armour2:3,400")
+
+                    print("Thanks for purchase")
+                else:
+                    print("Not enough cash")
+            
+            else:
+                print("I do not have the thing you are asking for")
+
+
+        elif user_buy == 5: # exit shop
             print("Thanks for visiting the shop\n")
             play_shop = False
             
-
         else: print("\nDid not get what you have wanted, repeat please")
 
     
     
     pass
+#----------------------------
+def Inventory(): # show inventory
+    global inventory
 
-def Inventory():
-    if len(inventory) == 0:
-        print("Your inventory has no items in it\nGo buy some stuff\n")
-    else:
-        for key, values in inventory.items():
-            print(key, values)
+    for key in inventory.keys():
+        print("List of ", key)
+        for item in inventory[key]:
+            print(item)
 
     pass
+#----------------------------
+def add_item(text,type): # add item to the inventory
+    global inventory
 
+    text_list = text.split(",")
+    if type[0].lower() == "w": # add to weapons
+        inventory["Weapons"].append({"name":text_list[0], "damage":int(text_list[1]), "price":int((text_list[2]))})
+
+    elif type[0].lower() == "k": # add to keys
+        inventory["Keys"].append({"code":text_list[0], "price":int(text_list[1])})
+
+    elif type[0].lower() == "a": # add to armours
+        inventory["Armours"].append({"Durability":int(text_list[0]), "price":int(text_list[1])})
+
+    else:
+        print("Adding the item <", text, "> into the invenory was unsuccessful")
+#----------------------------
+def change_labels(health, money, point): # change the values on the user window, whenever they happen  
+    health_label.configure(text= "Health: "+str(health))
+    money_label.configure(text= "Money: "+str(money))
+    point_label.configure(text= "Points: "+str(point))
+
+
+# ---------------------------------------------------------------
+# BUTTONS    (‾◡◝)
+# ---------------------------------------------------------------
 
 
 R1_btn = tkinter.Button(bottom_frame, text= "Room 1", command= Room1)
@@ -204,7 +278,7 @@ R3_btn.grid(row=0, column=1)
 R4_btn = tkinter.Button(bottom_frame, text= "Room 4", command= Room4)
 R4_btn.grid(row=1, column=1)
 
-Shp_btn = tkinter.Button(bottom_frame, text= "Shop", command= Shop)
+Shp_btn = tkinter.Button(bottom_frame, text= "   Shop   ", command= Shop)
 Shp_btn.grid(row=1, column=2)
 
 Inv_btn = tkinter.Button(bottom_frame, text= "Inventory", command= Inventory)
